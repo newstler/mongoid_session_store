a18561727e95f57ac515628178dd203cca38107c
Dimitri Kurashvili <dimitri@c12.ge>
2012-10-27 11:20:47 +0400

    gem name

--- lib/mongoid_session_store.rb
+++ lib/mongoid_session_store.rb
@@ -6,7 +6,7 @@ module MongoidSessionStore
     rake_tasks do
       load "tasks/mongoid_session_store_tasks.rake"
     end
-          
+
     initializer "setup mongoid session store" do |app|
       require 'mongoid_session_store/mongoid_store'
       app.config.session_store :mongoid_store
